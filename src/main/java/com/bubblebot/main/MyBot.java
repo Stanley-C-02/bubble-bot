77ef69478cb5dbe62daf14d39bdcc0a1e9ca521a
Stanley Chow <51177611+ForTheWindFTW@users.noreply.github.com>
2021-10-15 23:46:32 -0400

    initial commit

---
+++ src/main/java/com/bubblebot/main/MyBot.java
@@ -0,0 +1,15 @@
+package com.bubblebot.main;
+
+import discord4j.core.DiscordClient;
+
+public class MyBot {
+  public static void main(String[] args) {
+    // DiscordClient only provides operations while not logged in
+    DiscordClient client = DiscordClient.create("TOKEN");
+
+    // GatewayDiscordClient provides connection
+    Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) -> Mono.empty());
+
+    login.block();
+  }
+}
