be7f3dfaadf5720a039bb4033113cf7008fde00a
Stanley Chow <51177611+ForTheWindFTW@users.noreply.github.com>
2021-12-01 18:59:18 -0500

    developing Command object

--- src/main/java/com/bubblebot/main/Command.java
+++ src/main/java/com/bubblebot/main/Command.java
@@ -1,2 +1,15 @@
-package com.bubblebot.main;public class Command {
+package com.bubblebot.main;
+
+import java.util.function.Consumer;
+
+public class Command {
+  private String name;
+  private String[] alias;
+  Consumer<?> func;
+
+  public Command(final String name, final String[] alias, final Consumer<?> func) {
+    this.name = name;
+    this.alias = alias;
+    this.func = func;
+  }
 }
